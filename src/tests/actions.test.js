dc87e6296e568987ae31fa9879799d30d579475e
Elana <elanakoren@gmail.com>
2017-12-27 13:30:58 -0800

    add test for change gif order action

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -7,33 +7,46 @@ import rp from 'request-promise';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-describe('fetchGifs', () => {
-    let promise, response;
-    beforeEach(() => {
-        response = {
-            data: [
-                {type: "gif", id: "some-id-1", slug: "slug-1"},
-                {type: "gif", id: "some-id-2", slug: "slug-2"}
-            ],
-            meta: {status: 200, msg: "OK"},
-            pagination: {count: 25, offset: 0},
-        };
-        promise = Promise.resolve(response);
+describe('actions', () => {
+    describe('fetchGifs', () => {
+        let promise, response;
+        beforeEach(() => {
+            response = {
+                data: [
+                    {type: "gif", id: "some-id-1", slug: "slug-1"},
+                    {type: "gif", id: "some-id-2", slug: "slug-2"}
+                ],
+                meta: {status: 200, msg: "OK"},
+                pagination: {count: 25, offset: 0},
+            };
+            promise = Promise.resolve(response);
+        });
+
+        it('dispatches actions and passes data to the store', () => {
+            rp.get = jest.fn();
+            rp.get.mockReturnValue(promise);
+
+            const dispatchedActions = [
+                {type: types.FETCH_GIFS},
+                {type: types.RECEIVE_GIFS, gifs: response}
+            ];
+
+            const store = mockStore({gifs: []});
+
+            return store.dispatch(actions.fetchGifs()).then(() => {
+                expect(store.getActions()).toEqual(dispatchedActions)
+            });
+        });
     });
 
-    it('dispatches actions and passes data to the store', () => {
-        rp.get = jest.fn();
-        rp.get.mockReturnValue(promise);
-
-        const dispatchedActions = [
-            {type: types.FETCH_GIFS},
-            {type: types.RECEIVE_GIFS, gifs: response}
-        ];
-
-        const store = mockStore({gifs: []});
-
-        return store.dispatch(actions.fetchGifs()).then(() => {
-            expect(store.getActions()).toEqual(dispatchedActions)
+    describe('changeGifOrder', () => {
+        it('should create an action to change the sort order', () => {
+            const order = 'desc';
+            const expectedAction = {
+                type: types.CHANGE_GIF_ORDER,
+                order
+            };
+            expect(actions.changeGifOrder(order)).toEqual(expectedAction)
         });
     });
 });
