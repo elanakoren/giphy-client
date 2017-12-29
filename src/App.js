fd8b698e8a7a4f26a37b4623df6148c2fee44774
Elana <elanakoren@gmail.com>
2017-12-28 21:44:12 -0800

    show a warning if API key is missing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import GifListContainer from './components/gifListContainer';
 
 class App extends Component<{||}> {
     render() {
+        if (process.env.REACT_APP_API_KEY === undefined) {
+            window.alert('Please run the app with your giphy API key set as an environment variable ("REACT_APP_API_KEY"). See the readme.');
+        }
         return (
             <div className="App">
                 <GifListContainer/>
