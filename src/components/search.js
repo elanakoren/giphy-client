63201dedb17399fd0b8e4165dc7694c4bda38b52
Elana <elanakoren@gmail.com>
2017-12-30 10:21:22 -0800

    user can reset searches

--- src/components/search.js
+++ src/components/search.js
@@ -13,6 +13,15 @@ export default class Search extends Component<Props> {
         this.searchQuery = null;
     }
 
+    clearSearch() {
+        const {gifActions} = this.props;
+        if (this.searchQuery instanceof HTMLInputElement) {
+            this.searchQuery.value = '';
+        }
+        gifActions.fetchGifs();
+
+    }
+
     render() {
         const {gifActions} = this.props;
         return (
@@ -26,6 +35,11 @@ export default class Search extends Component<Props> {
                     className="search-button"
                     onClick={() => gifActions.fetchGifs(this.searchQuery)}>
                     Search
+                </button>
+                <button
+                    className="search-button"
+                    onClick={() => this.clearSearch()}>
+                    Clear Search
                 </button>
             </div>
         )
